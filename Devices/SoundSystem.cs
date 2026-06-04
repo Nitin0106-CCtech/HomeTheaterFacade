f2a71a33ca9e0aeed2ad73c8bf474181face6a4d
Nitin0106-CCtech <nitin.kumar@o365.cctech.co.in>
2026-06-04 10:51:24 +0530

    Create SoundSystem.cs

---
+++ Devices/SoundSystem.cs
@@ -0,0 +1,19 @@
+namespace HomeTheaterFacade.Devices;
+
+public class SoundSystem
+{
+    public void On()
+    {
+        Console.WriteLine("Sound System ON");
+    }
+
+    public void SetVolume(int level)
+    {
+        Console.WriteLine($"Volume set to {level}");
+    }
+
+    public void Off()
+    {
+        Console.WriteLine("Sound System OFF");
+    }
+}
