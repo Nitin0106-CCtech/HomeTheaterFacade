158d6ec858b0c414c0a38784a92fbabd747baf56
Nitin0106-CCtech <nitin.kumar@o365.cctech.co.in>
2026-06-04 10:50:53 +0530

    Add Screen class with Up and Down methods

---
+++ Devices/Screen.cs
@@ -0,0 +1,14 @@
+namespace HomeTheaterFacade.Devices;
+
+public class Screen
+{
+    public void Down()
+    {
+        Console.WriteLine("Screen Down");
+    }
+
+    public void Up()
+    {
+        Console.WriteLine("Screen Up");
+    }
+}
