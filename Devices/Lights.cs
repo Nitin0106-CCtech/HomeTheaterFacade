66e17389dbb596a32c6bc194963e8c5250828ada
Nitin0106-CCtech <nitin.kumar@o365.cctech.co.in>
2026-06-04 10:49:51 +0530

    Add Lights class for controlling lighting

---
+++ Devices/Lights.cs
@@ -0,0 +1,14 @@
+namespace HomeTheaterFacade.Devices;
+
+public class Lights
+{
+    public void Dim()
+    {
+        Console.WriteLine("Lights Dimmed");
+    }
+
+    public void On()
+    {
+        Console.WriteLine("Lights ON");
+    }
+}
